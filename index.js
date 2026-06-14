65e24532fe87c8d7c7ba05a3c6235e10847ae38f
vanessamzuniga-beep <vanessamzuniga@gmail.com>
2026-06-14 16:22:38 -0500

    Lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your code here!
+
+// display fetched data
+async function displayPosts(posts) {
+    const postList = document.querySelector('#post-list')
+    posts.forEach(post => {
+        const listItem = document.createElement('li')
+        const heading = document.createElement('h1')
+        heading.textContent = post.title
+
+        const paragraph = document.createElement('p')
+        paragraph.textContent = post.body
+        listItem.appendChild(heading)
+        listItem.appendChild(paragraph)
+
+        postList.appendChild(listItem)
+    })
+}
+
+
+// fetch data
+async function fetchPosts() {
+
+    try{
+        
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+
+        const posts = await response.json()
+
+        displayPosts(posts)
+
+        return posts
+    } catch (error) {
+    console.error("Error fetching data:", error)
+    }
+}
+fetchPosts()
